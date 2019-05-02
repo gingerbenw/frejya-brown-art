3eaf727c28c96e81d09e521ef108a46c6ed6c65e
Ginger Ben <ben.wilson@thevirtualforge.com>
2019-05-02 11:37:36 +0100

    add html lang tag

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,14 @@ import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
-    );
+    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
   }
 
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="/static/css/main.css" rel="stylesheet" />
