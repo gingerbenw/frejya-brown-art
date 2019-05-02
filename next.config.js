6408cc8c1c607980c02bad1e0ebb76d793aa9d02
Ginger Ben <ben.wilson@thevirtualforge.com>
2019-05-02 11:28:55 +0100

    update config again

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-underscore-dangle */
-
-// require('dotenv').config();
-// const withCSS = require('@zeit/next-css');
-// const { createApolloFetch } = require('apollo-fetch');
-
-// const apolloFetch = createApolloFetch({ uri: 'http://localhost:1337/graphql' });
-
-const artworks = require('./data');
-
 module.exports = {
   webpack: (config) => {
     config.module.rules.push(
@@ -24,22 +13,8 @@ module.exports = {
     return config;
   },
   exportPathMap: async () => {
-    //   // Get list of pages from API and map them to 'real' pages/templates
-    //   const artworkPages = artworks.reduce(
-    //     (pages, { url, id }) =>
-    //       Object.assign({}, pages, {
-    //         [url]: {
-    //           page: '/artwork',
-    //           query: { id },
-    //         },
-    //       }),
-    //     {},
-    //   );
-
-    // Combine the page maps and any static pages
     const exportPages = {
-      // ...artworkPages,
-      // '/': { page: '/' },
+      //   @TODO: Get list of pages from API and map them to 'real' pages/templates
       '/': { page: '/home' },
       '/art/jon-snow': {
         page: '/art',
