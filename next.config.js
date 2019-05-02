04fe0c0b194ce1424a4577625859c4dc6fd76919
Ginger Ben <ben.wilson@thevirtualforge.com>
2019-05-02 20:32:37 +0100

    stuff

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+/* eslint-disable prefer-template */
 module.exports = {
   webpack: (config) => {
     config.module.rules.push(
@@ -13,15 +17,27 @@ module.exports = {
     return config;
   },
   exportPathMap: async () => {
+    const directoryPath = path.join(__dirname, 'content/artworks');
+
+    const artwork = await new Promise((resolve, reject) => {
+      fs.readdir(directoryPath, (err, data) => {
+        const files = data.reduce(
+          (artworks, file) => Object.assign({}, artworks, {
+              ['/art/' + file.split('.md')[0]]: {
+                page: '/art',
+                query: { file },
+              },
+            }),
+          {},
+        );
+        if (err) reject(err);
+        else resolve(files);
+      });
+    });
+
     const exportPages = {
-      //   @TODO: Get list of pages from API and map them to 'real' pages/templates
       '/': { page: '/home' },
-      '/art/jon-snow': {
-        page: '/art',
-        query: {
-          slug: '2019-05-02-jon-snow',
-        },
-      },
+      ...artwork,
     };
 
     return exportPages;
--- pages/art.js
+++ pages/art.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { withRouter } from 'next/router';
-
-import Content from 'components/Content';
+import PropTypes from 'prop-types';
+import Content from '../components/Content';
 
 const Artwork = ({ content }) => {
   const {
     html,
-    attributes: { title, featuredImage, date, excerpt, meta },
+    attributes: { title, featuredImage },
   } = content;
   return (
     <div>
       <h1>{title}</h1>
       <Content src={html} />
-      <img src={featuredImage} />
+      <img src={featuredImage} alt={title} />
     </div>
   );
 };
 
 export default Artwork;
 
-Artwork.getInitialProps = ({ query: { slug } }) => {
-  const content = require(`../content/artworks/${slug}.md`);
+Artwork.propTypes = {
+  content: PropTypes.object,
+};
+
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+Artwork.getInitialProps = ({ query: { file } }) => {
+  const content = require(`../content/artworks/${file}`);
   return { content };
 };
